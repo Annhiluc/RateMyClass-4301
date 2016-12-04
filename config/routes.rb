83b5788bdc59ff037e7d9c22829303141754fc53
Minna1Hassan <minn.hassan@gmail.com>
2016-12-04 16:35:13 -0500

    Removed map to make the application work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'map/show'
+  # get 'map/show'
   get 'student/new'
   post 'student/create'
   patch 'student/update'
