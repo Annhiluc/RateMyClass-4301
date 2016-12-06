f7c9c06eb058f1fc798f3d8b75d5596164927ec4
Annie Luc <annhiluc>
2016-12-06 16:22:13 -0500

    Finalized website

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -10,30 +10,4 @@ class MapController < ApplicationController
                 @result = ActiveRecord::Base.connection.exec_query(sql)
                 
     end
-    
-          #sql = " SELECT AVG(average_interest)
-           #       FROM school s, department d, class c, ratecourse r
-            #      WHERE s.school_id = d.school_id, c.dept_id = d.dept_id c.course_id = r.course_id  
-             #     GROUP BY s.state"
-              #    @result = ActiveRecord::Base.connection.exec_query(sql)
-  
-  
-          #each state gets a marker 50 states total
-  
-          #California
-        #  var marker1 = L.marker([36.7783, 119.4179]).addTo(mymap);
-        ## marker1.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-  
-          ##Pennsylvania
-          #var marker2 = L.marker([41.2033, 77.1945]).addTo(mymap);
-          #marker2.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-  
-          #Florida
-          #var marker3 = L.marker([27.6648, 81.5158]).addTo(mymap);
-          #marker3.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-  
-          #Minnesota
-          #var marker4 = L.marker([46.7296, 94.6859]).addTo(mymap);
-          #marker4.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-    #end
 end   
